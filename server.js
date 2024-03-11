f5b16d68b0f780eeafad6b4e9fcf62081998b393
MarcJ <144659851+jauniauMarc@users.noreply.github.com>
2024-03-11 09:10:48 +0000

    retouche sign in

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
-const { spawnSync } = require('child_process');
-const { pseudoRandomBytes } = require('crypto');
 const express = require('express')
 const path = require('path');
+const cors = require('cors');
 const app = express()
 
-
-
+const port = app.get('port') || 3003; // Use port 3003 as a default if not set
 app.use(express.static('.'));
 app.use(express.json())
+// Enable CORS for all routes (you can customize the options)
+app.use(cors());
 
 
-// Définir la route http://127.0.0.1:3003/ pour index.html
-app.get('http://127.0.0.1:3001/', (req, res) => {
+//  http://127.0.0.1:3003/ 
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
 
-  // Définir la route http://127.0.0.1:3003/todo pour indexTodo.html
-app.get('http://127.0.0.1:3001/todo', (req, res) => {
-  res.sendFile(path.join(__dirname, 'indexTodo.html'));
+// http://127.0.0.1:3003/signIn 
+app.get('/signIn', (req, res) => {
+  res.sendFile(path.join(__dirname, 'signIn.html'));
 });
 
- 
+// http://127.0.0.1:3003/signUp 
+app.get('/signUp', (req, res) => {
+  res.sendFile(path.join(__dirname, 'signUp.html'));
+});
 
+//Users @ http://127.0.0.1:3003/get/user
+app.get('/get/user/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'accounts.json'));
+});
 
+http://localhost:3003/get/user/data/profil
+app.get('/get/user/data/profil/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dataUsers.json'));
+});
 
- 
-
-
-
-
-app.listen(3003)
+app.listen(port)
